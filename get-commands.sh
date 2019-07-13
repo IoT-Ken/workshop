3dd67bd61c1e5727fa2b8e7467c5002b5db74648
IoT-Ken <48492944+IoT-Ken@users.noreply.github.com>
2019-07-13 14:03:48 -0500

    Add files via upload

---
+++ get-commands.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#############################################################################
+# Filename: get-commands.sh
+# Date Created: 07/13/19
+# Date Modified: 07/13/19
+# Author: Ken Osborn
+#
+# Version 1.0
+#
+# Description: Used during Workshop to force Pulse Agent to phone home every
+#              10 seconds during Campaign Lesson.
+#
+# Usage: ./get-commands.sh
+#
+# 1.0 - Ken Osborn: First version of the script.
+#############################################################################
+
+################################################################################
+## Set Variables
+################################################################################
+AGENTBINPATH="/opt/vmware/iotc-agent/bin/"
+
+#Start While Loop
+while true; do
+
+#Issue get-commands using DefaultClient
+${AGENTBINPATH}DefaultClient get-commands
+
+# Configure Interval
+sleep 15
+done
