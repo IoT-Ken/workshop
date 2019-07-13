b3ef6272e435ea46c1423c59268919afb5ab79f3
IoT-Ken <48492944+IoT-Ken@users.noreply.github.com>
2019-07-12 19:32:43 -0500

    Update modifyCommandFetchInterval.sh

--- modifyCommandFetchInterval.sh
+++ modifyCommandFetchInterval.sh
@@ -63,7 +63,3 @@ echo '{
   http --verify=no PUT https://$PULSEINSTANCE:443/api/device-templates/$(cat ${AGENTDATAPATH}vGatewayTemplate.id | awk -F ':' '{print $2}' | sed -e 's/"//g' | sed -e 's/}//g') \
   Accept:'application/json;api-version=1.0' \
   Authorization:"Bearer $BearerToken"
-  Host:$PULSEINSTANCE:443 \
-  accept-encoding:'gzip, deflate' \
-
-  
